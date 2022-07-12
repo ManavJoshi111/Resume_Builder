1fe97f67bd47ea2f1cd91258c17bb9ace693decc
ManavJoshi <manavjoshi154@gmail.com>
2022-07-12 22:37:46 +0530

    make it better

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "../Styles/header.css";
-import Fade from "react-reveal/Fade";
 import image from "../Images/resume_cover.png";
 
 const Header = (props) => {
@@ -28,38 +27,36 @@ const Header = (props) => {
   }, []);
   return (
     <div className="container-fluid mt-3 flex-wrap d-flex flex-column-reverse flex-md-row justify-content-center align-items-center">
-      <Fade bottom>
-        <div className="leftcontainer ps-5">
-          <h2 id="hey" className="fw-bold mt-2">
-            Create Your <span> Professional</span> Resume For Free
-          </h2>
-          <p id="para" className="fw-lighter fs-5">
-            Make yourself out of crowd using our resume builder.
-            <br />
-            We provide you with a professional resume that is designed to fit in
-            with your style.
-          </p>
-          <center>
-            <NavLink
-              to="/makecv"
-              className="btn btn-primary btn-block mt-3"
-            >
-              Get Started
-            </NavLink>
-          </center>
-        </div>
-        <div className="rightcontainer">
-          <center>
-            <img
-              id="imganim"
-              // src="https://i.pinimg.com/originals/b5/bb/80/b5bb80994bc3ecdcd5b989250e6b7746.png"
-              src={image}
-              className="img-fluid"
-              alt=""
-            />
-          </center>
-        </div>
-      </Fade>
+      <div className="leftcontainer ps-5">
+        <h2 id="hey" className="fw-bold mt-2">
+          Create Your <span> Professional</span> Resume For Free
+        </h2>
+        <p id="para" className="fw-lighter fs-5">
+          Make yourself out of crowd using our resume builder.
+          <br />
+          We provide you with a professional resume that is designed to fit in
+          with your style.
+        </p>
+        <center>
+          <NavLink
+            to="/makecv"
+            className="btn btn-primary btn-block mt-3"
+          >
+            Get Started
+          </NavLink>
+        </center>
+      </div>
+      <div className="rightcontainer">
+        <center>
+          <img
+            id="imganim"
+            // src="https://i.pinimg.com/originals/b5/bb/80/b5bb80994bc3ecdcd5b989250e6b7746.png"
+            src={image}
+            className="img-fluid"
+            alt=""
+          />
+        </center>
+      </div>
     </div>
   );
 };
